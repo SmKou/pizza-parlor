d37b5fd674edf0e91028b06e6c111051b890dfa6
SmKou <66178856+SmKou@users.noreply.github.com>
2023-06-25 16:24:11 -0700

    WIP Add UI logic

---
+++ src/js/ui/scripts.js
@@ -0,0 +1,25 @@
+window.onload = load;
+
+function load() {
+    const form = document.querySelector('form');
+    form.addEventListener('submit', handleFormSubmission);
+    form.addEventListener('reset', resetForm);
+}
+
+function handleFormSubmission() { 
+    const size = document.querySelector('[name="size"]:checked').value;
+    const sauce = [];
+    document.querySelectorAll('[name="sauce"]:checked').forEach(elem => sauce.push(elem.value));
+    const cheese = [];
+    document.querySelectorAll('[name="cheese"]:checked').forEach(elem => cheese.push(elem.value));
+    const veggies = [];
+    document.querySelectorAll('[name="veggies"]:checked').forEach(elem => veggies.push(elem.value));
+    const protein = [];
+    document.querySelectorAll('[name="protein"]:checked').forEach(elem => protein.push(elem.value));
+
+    const pizza = new Pizza(size, sauce, cheese, veggies, protein);
+}
+
+function resetForm(e) {
+    console.log(e.target);
+}
